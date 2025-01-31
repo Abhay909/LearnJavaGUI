261a7fb6d4a700130c9fe6941217337f658b232b
abhay909 <a9kapatkar@gmail.com>
2025-01-30 20:28:23 -0800

    Simple added image.jpeg and labels and everything up till this point

--- src/Main.java
+++ src/Main.java
@@ -9,7 +9,7 @@ public class Main {
     public static void main(String[] args) {
 
 
-        MyFrame frame = new MyFrame();
+        MyFrame frame = new MyFrame();//
         JLabel label = new JLabel("Hello World");
         frame.add(label);
 
