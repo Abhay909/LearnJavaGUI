a505501d667c5c0d14d9bdcedf811e9544decdfa
Abhay Kapatkar <a9kapatkar@gmail.com>
2025-02-01 13:06:38 -0800

    Update gui.java
    Signed-off-by: Abhay Kapatkar <a9kapatkar@gmail.com>

--- src/JavaButtons/gui.java
+++ src/JavaButtons/gui.java
@@ -7,34 +7,6 @@ public class gui {
 
         JButtonApp app = new JButtonApp();
         app.setVisible(true); // Good habit of setting it to true to make sure everything appears
-        
-//
-//        JFrame window = new JFrame();
-//
-//        window.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-//
-//        window.setTitle("Java GUI Buttons");
-//        window.setSize(400, 400);
-//        window.setLocationRelativeTo(null);
-//
-//        // Add Button
-//        JButton button = new JButton("button");
-//
-//
-//        // Action Listener (action of the button)
-//        button.addActionListener(new ActionListener() {
-//            @Override
-//            public void actionPerformed(ActionEvent e) {
-//                System.out.println("Hello!");
-//            }
-//        });
-//
-// //
-//
-//        window.add(button);
-//
-//        // end
-//        window.setVisible(true);
 
     }
 }
